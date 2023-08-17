384d53c211b8e1c959534ac4e107bd7c12e7de30
maruf12 <maruf.harsono@gmail.com>
2023-08-17 20:54:11 +0700

    chore: install vee-validate (still error)

---
+++ plugins/vee-validate.ts
@@ -0,0 +1,18 @@
+import { configure, defineRule } from "vee-validate";
+import AllRules from "@vee-validate/rules";
+import { localize, setLocale } from "@vee-validate/i18n";
+import id from "@vee-validate/i18n/dist/locale/id.json";
+
+export default defineNuxtPlugin((_nuxtApp) => {
+  Object.keys(AllRules).forEach((rule) => {
+    defineRule(rule, AllRules[rule]);
+  });
+
+  configure({
+    generateMessage: localize({
+      id,
+    }),
+  });
+
+  setLocale("id");
+});
